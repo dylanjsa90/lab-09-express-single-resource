5fd70e0b77385b8967d402c86db816c4362ebc6e
dylanjsa90 <dylanjsanders1@gmail.com>
2016-09-23 18:51:22 -0700

    added debug log to middleware

--- lab-dylan-sanders/lib/error_response.js
+++ lab-dylan-sanders/lib/error_response.js
@@ -1,8 +1,11 @@
 const AppError = require('./AppError');
+const debug = require('debug');
+const errorLog = debug('errorLog');
 
 module.exports = exports = function() {
   return (req, res, next) => {
     res.sendError = function(err) {
+      errorLog('error ' + err.statusCode + ' msg: ' + err.responseMessage);
       if (AppError.isAppError(err)) {
         return res.status(err.statusCode).send(err.responseMessage);
       } else {
